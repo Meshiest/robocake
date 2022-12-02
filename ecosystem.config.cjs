201cdbe4030d2068465073dcd611373f379817d9
Meshiest <gpg@reheatedcake.io>
2022-12-01 21:21:05 -0600

    move code to src/

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'discord',
-      script: './main.js',
+      script: './src/main.js',
       watch: true,
       //  don't for get to put `TOKEN=<TOKEN>` in .env
     },
